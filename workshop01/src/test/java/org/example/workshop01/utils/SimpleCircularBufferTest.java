872e32f86d09b175892e97dc700b9acbf37fa5bb
Pasakorn Tongsan <pasakorn.t@kbtg.tech>
2021-02-08 14:21:09 +0700

    Add specifig size test

--- workshop01/src/test/java/org/example/workshop01/utils/SimpleCircularBufferTest.java
+++ workshop01/src/test/java/org/example/workshop01/utils/SimpleCircularBufferTest.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleCircularBufferTest {
 
-    @DisplayName("Default size should be 10")
+    CircularBuffer cb = new SimpleCircularBuffer();
+
     @Test
+    @DisplayName("Default size should be 10")
     public void create_buffer_with_default_size(){
-        CircularBuffer cb = new SimpleCircularBuffer();
+        cb.create();
         assertEquals(10, cb.getSize());
     }
 
+    @Test
+    @DisplayName("Create with size 5 should be 5 by specified")
+    public void create_buffer_with_specific_size(){
+        final int SPECIFIC_SIZE = 5;
+        cb.create(SPECIFIC_SIZE);
+        assertEquals(SPECIFIC_SIZE, cb.getSize());
+    }
 }
