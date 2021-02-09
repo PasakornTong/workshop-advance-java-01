c6cab648ddab9f5518cf242507bca6ccd6591bba
Pasakorn Tongsan <pasakorn.t@kbtg.tech>
2021-02-09 14:38:46 +0700

    Add workshop03

--- workshop02/src/main/java/org/example/workshop02/model/Buyer.java
+++ workshop02/src/main/java/org/example/workshop02/model/Buyer.java
@@ -6,21 +6,26 @@ import org.example.workshop02.service.Checkout;
 public class Buyer {
     private String firstName;
     private String lastName;
+    private Basket basket;
+
+    public Buyer(){
+        this.basket = new Basket();
+    }
 
     public void requestToBuy() {
         // 1. Create basket
-        Basket basket = new Basket();
-        // 2. Add book to basket
-        Book book1 = new Book("Potter 1", 8);
-        Book book2 = new Book("Potter 2", 8);
-        Book book3 = new BookBuilder().chooseBook("Potter 3").build();
-        basket.addBook(book1);
-        basket.addBook(book2);
-        basket.addBook(book3);
-        // 3. Checkout
-        // TODO :: checkout process
-        Checkout checkout = new Checkout();
-        checkout.process(basket);
+//        Basket basket = new Basket();
+//        // 2. Add book to basket
+//        Book book1 = new Book("Potter 1", 8);
+//        Book book2 = new Book("Potter 2", 8);
+//        Book book3 = new BookBuilder().chooseBook("Potter 3").build();
+//        basket.addBook(book1);
+//        basket.addBook(book2);
+//        basket.addBook(book3);
+//        // 3. Checkout
+//        // TODO :: checkout process
+//        Checkout checkout = new Checkout();
+//        checkout.process(basket);
 
         // Check netPrice = 24, discountPrice 24 - 10%
     }
---
+++ workshop02/src/main/java/org/example/workshop02/model/Item.java
@@ -0,0 +1,7 @@
+package org.example.workshop02.model;
+
+import java.util.List;
+
+public class Item {
+    private List<Book> books;
+}
